4d46d76134eb1c2bab95a7af885d5b8b0b88966d
Vinh Chu <vinhchugg@gmail.com>
2021-10-06 18:57:42 +0100

    Add the properties and methods to card.js.

---
+++ src/card.js
@@ -0,0 +1,30 @@
+const Cards = function () {
+  this.suits = ["Diamond", "Colver", "Heart", "Spade"];
+  this.values = [14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2];
+  this.deck = [];
+};
+
+Cards.prototype.createDeck = function () {
+  for (let suit = 0; suit < this.suits.length; suit++) {
+    for (let value = 0; value < this.values.length; value++) {
+      const card = {
+        suit: this.suits[suit],
+        value: this.values[value],
+      };
+      this.deck.push(card);
+    }
+  }
+  return this.deck;
+};
+
+Cards.prototype.shuffle = function () {
+  let m = this.deck.length;
+  while (m) {
+    let i = Math.floor(Math.random() * m--);
+
+    [this.deck[m], this.deck[i]] = [this.deck[i], this.deck[m]];
+  }
+  return this.deck;
+};
+
+const cards = new Cards();
---
+++ src/player.js
@@ -0,0 +1,6 @@
+const Player = function (name) {
+  this.name = name;
+  this.hand = null;
+};
+
+Player.prototype.countCards = function () {};
