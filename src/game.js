dd7a618b9bac3b553e1602e13fa262682fa2f561
Vinh Chu <vinhchugg@gmail.com>
2021-10-07 16:16:18 +0100

    Add win function for war to game.js

--- src/game.js
+++ src/game.js
@@ -24,90 +24,101 @@ function specialCards(value) {
 }
 
 Game.prototype.playWar = function () {
-  // Two empty arrays to store the 4 cards
-  let warCard0 = [];
-  let warCard1 = [];
-
-  // Check if still playing previous War
-  if (!this.betWarCards) {
-    // Push first four items of player's hand to war cards
-    warCard0.push(...player0.hand.splice(0, 4));
-    warCard1.push(...player1.hand.splice(0, 4));
-
-    // Put cards in war pot
-    this.betWarCards = [...warCard0, ...warCard1];
-
-    // Take first card from player's war cards
-    let player0Card = warCard0.shift();
-    let player1Card = warCard1.shift();
-
-    // If player 1 wins
-    if (player0Card.value > player1Card.value) {
-      player0.hand.push(...this.betWarCards);
-      warCard0 = [];
-      warCard1 = [];
-      this.betWarCards = undefined;
-      this.inWar = false;
-      return `Player 1 has ${specialCards(player0Card.value)} of ${
-        player0Card.suit
-      } which is bigger than ${specialCards(player1Card.value)} of ${
-        player1Card.suit
-      }. Thus Player 1 has won the War!`;
-    }
-
-    // If player 2 wins
-    else if (player0Card.value < player1Card.value) {
-      player1.hand.push(...this.betWarCards);
-      warCard0 = [];
-      warCard1 = [];
-      this.betWarCards = undefined;
-      this.inWar = false;
-      return `Player 2 has ${specialCards(player1Card.value)} of ${
-        player1Card.suit
-      } which is bigger than ${specialCards(player0Card.value)} of ${
-        player0Card.suit
-      }. Thus Player 2 has won the War!`;
-    }
-
-    // Cards are similar, check next card
-    else {
-      console.log("The war continues");
-      return Game.prototype.playRound();
-    }
+  if (player0.hand.length < 4) {
+    this.isPlaying = false;
+    return "Player 2 has less than 4 cards, Player 1 has won the game!";
   }
-  if (warCard0.length > 0 || warCard1.length > 0) {
-    if (player0Card.value > player1Card.value) {
-      player0.hand.push(...this.betWarCards);
-      warCard0 = [];
-      warCard1 = [];
-      this.betWarCards = undefined;
-      this.inWar = false;
-      console.log(this);
-      return `Player 1 has ${specialCards(player0Card.value)} of ${
-        player0Card.suit
-      } which is bigger than ${specialCards(player1Card.value)} of ${
-        player1Card.suit
-      }. Thus Player 1 has won the War!`;
-    }
+  if (player1.hand.length < 4) {
+    this.isPlaying = false;
+    return "Player 1 has less than 4 cards, Player 2 has won the game!";
+  }
 
-    // If player 2 wins
-    else if (player0Card.value < player1Card.value) {
-      player1.hand.push(...this.betWarCards);
-      warCard0 = [];
-      warCard1 = [];
-      this.betWarCards = undefined;
-      this.inWar = false;
-      return `Player 2 has ${specialCards(player1Card.value)} of ${
-        player1Card.suit
-      } which is bigger than ${specialCards(player0Card.value)} of ${
-        player0Card.suit
-      }. Thus Player 2 has won the War!`;
-    }
+  if (this.isPlaying) {
+    // Two empty arrays to store the 4 cards
+    let warCard0 = [];
+    let warCard1 = [];
 
-    // Cards are similar, check next card
-    else {
-      console.log("The war continues");
-      return Game.prototype.playRound();
+    // Check if still playing previous War
+    if (!this.betWarCards) {
+      // Push first four items of player's hand to war cards
+      warCard0.push(...player0.hand.splice(0, 4));
+      warCard1.push(...player1.hand.splice(0, 4));
+
+      // Put cards in war pot
+      this.betWarCards = [...warCard0, ...warCard1];
+
+      // Take first card from player's war cards
+      let player0Card = warCard0.shift();
+      let player1Card = warCard1.shift();
+
+      // If player 1 wins
+      if (player0Card.value > player1Card.value) {
+        player0.hand.push(...this.betWarCards);
+        warCard0 = [];
+        warCard1 = [];
+        this.betWarCards = undefined;
+        this.inWar = false;
+        return `Player 1 has ${specialCards(player0Card.value)} of ${
+          player0Card.suit
+        } which is bigger than ${specialCards(player1Card.value)} of ${
+          player1Card.suit
+        }. Thus Player 1 has won the War!`;
+      }
+
+      // If player 2 wins
+      else if (player0Card.value < player1Card.value) {
+        player1.hand.push(...this.betWarCards);
+        warCard0 = [];
+        warCard1 = [];
+        this.betWarCards = undefined;
+        this.inWar = false;
+        return `Player 2 has ${specialCards(player1Card.value)} of ${
+          player1Card.suit
+        } which is bigger than ${specialCards(player0Card.value)} of ${
+          player0Card.suit
+        }. Thus Player 2 has won the War!`;
+      }
+
+      // Cards are similar, check next card
+      else {
+        console.log("The war continues");
+        return Game.prototype.playRound();
+      }
+    }
+    if (warCard0.length > 0 || warCard1.length > 0) {
+      if (player0Card.value > player1Card.value) {
+        player0.hand.push(...this.betWarCards);
+        warCard0 = [];
+        warCard1 = [];
+        this.betWarCards = undefined;
+        this.inWar = false;
+        console.log(this);
+        return `Player 1 has ${specialCards(player0Card.value)} of ${
+          player0Card.suit
+        } which is bigger than ${specialCards(player1Card.value)} of ${
+          player1Card.suit
+        }. Thus Player 1 has won the War!`;
+      }
+
+      // If player 2 wins
+      else if (player0Card.value < player1Card.value) {
+        player1.hand.push(...this.betWarCards);
+        warCard0 = [];
+        warCard1 = [];
+        this.betWarCards = undefined;
+        this.inWar = false;
+        return `Player 2 has ${specialCards(player1Card.value)} of ${
+          player1Card.suit
+        } which is bigger than ${specialCards(player0Card.value)} of ${
+          player0Card.suit
+        }. Thus Player 2 has won the War!`;
+      }
+
+      // Cards are similar, check next card
+      else {
+        console.log("The war continues");
+        return Game.prototype.playRound();
+      }
     }
   }
 };
