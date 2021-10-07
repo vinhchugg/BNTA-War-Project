82e470551dc71a4ff4a0864d08f3232e30d2003f
Vinh Chu <vinhchugg@gmail.com>
2021-10-07 11:22:04 +0100

    Add playround prototype to game.js.

--- src/game.js
+++ src/game.js
@@ -1,5 +1,7 @@
 const Game = function () {
   this.isPlaying = true;
+  this.inWar = false;
+  this.betCards = [];
 };
 
 Game.prototype.dealCards = function () {
@@ -19,3 +21,37 @@ function specialCards(value) {
   if (value === 11) return "Jack";
   return value;
 }
+
+Game.prototype.playRound = function () {
+  // If not in war
+  if (this.inWar === false) {
+    // Both players draw cards
+    let player0Card = player0.hand.shift();
+    let player1Card = player1.hand.shift();
+
+    // Cards get put into a pot to be given to the winner
+    this.betCards.push(player0Card, player1Card);
+
+    // If player 1 wins
+    if (player0Card.value > player1Card.value) {
+      player0.hand.push(...this.betCards);
+      this.betCards = [];
+      return `Player 1 has ${specialCards(player0Card.value)} of ${
+        player0Card.suit
+      } which is bigger than ${specialCards(player1Card.value)} of ${
+        player1Card.suit
+      }`;
+    }
+
+    // If player 2 wins
+    if (player0Card.value < player1Card.value) {
+      player1.hand.push(...this.betCards);
+      this.betCards = [];
+      return `Player 2 has ${specialCards(player1Card.value)} of ${
+        player1Card.suit
+      } which is bigger than ${specialCards(player0Card.value)} of ${
+        player0Card.suit
+      }`;
+    }
+  }
+};
